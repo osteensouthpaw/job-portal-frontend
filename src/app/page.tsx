2920c91ec49e2748153b5bd483d5d905658eed75
osteensouthpaw <osteensouthpaw@gmail.com>
2025-03-11 22:54:14 +0300

    add custom container component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { Container } from "@/components/ui/container";
 
 export default function Home() {
   return (
-    <div>
+    <Container variant="constrainedPadded">
       <Button>Hello World</Button>
       <p>Hello World</p>
-    </div>
+    </Container>
   );
 }
