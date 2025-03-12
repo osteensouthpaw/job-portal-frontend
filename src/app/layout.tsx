13e249fe8ede4b6a6229f79bc314169f0c8e3c97
osteensouthpaw <osteensouthpaw@gmail.com>
2025-03-12 11:39:51 +0300

    style the navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NavBar />
-        <main>{children}</main>
+        <main className="text-zinc-700">{children}</main>
       </body>
     </html>
   );
--- src/components/general/NavBar.tsx
+++ src/components/general/NavBar.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Button } from "../ui/button";
+import Image from "next/image";
+import Logo from "@/public/logo.png";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
     <nav className="flex flex-row justify-between items-center border-b py-2">
-      <h1>Logo</h1>
+      <Link href="/" className="flex flex-row items-center gap-2">
+        <Image src={Logo} alt="logo" width={40} height={40} />
+        <h1 className="font-bold text-2xl hidden md:inline-flex">
+          Jobs<span className="text-primary font-semibold">:Mega</span>
+        </h1>
+      </Link>
+
       <ul className="flex flex-row gap-4 items-center">
         <li>Theme</li>
         <li>
