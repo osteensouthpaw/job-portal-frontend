289da4043871e4686a650dfe21d0fc46d4159d74
osteensouthpaw <osteensouthpaw@gmail.com>
2025-03-12 11:45:29 +0300

    implement response layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/general/NavBar";
+import { Container } from "@/components/ui/container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NavBar />
-        <main className="text-zinc-700">{children}</main>
+        <main className="text-zinc-700">
+          <Container variant="constrainedPadded">{children}</Container>
+        </main>
       </body>
     </html>
   );
--- src/components/general/NavBar.tsx
+++ src/components/general/NavBar.tsx
@@ -3,25 +3,28 @@ import { Button } from "../ui/button";
 import Image from "next/image";
 import Logo from "@/public/logo.png";
 import Link from "next/link";
+import { Container } from "../ui/container";
 
 const NavBar = () => {
   return (
-    <nav className="flex flex-row justify-between items-center border-b py-2">
-      <Link href="/" className="flex flex-row items-center gap-2">
-        <Image src={Logo} alt="logo" width={40} height={40} />
-        <h1 className="font-bold text-2xl hidden md:inline-flex">
-          Jobs<span className="text-primary font-semibold">:Mega</span>
-        </h1>
-      </Link>
+    <Container variant="constrainedPadded">
+      <nav className="flex flex-row justify-between items-center border-b py-2">
+        <Link href="/" className="flex flex-row items-center gap-2">
+          <Image src={Logo} alt="logo" width={40} height={40} />
+          <h1 className="font-bold text-2xl hidden md:inline-flex">
+            Jobs<span className="text-primary font-semibold">:Mega</span>
+          </h1>
+        </Link>
 
-      <ul className="flex flex-row gap-4 items-center">
-        <li>Theme</li>
-        <li>
-          <Button>Post Job</Button>
-        </li>
-        <li>Profile</li>
-      </ul>
-    </nav>
+        <ul className="flex flex-row gap-4 items-center">
+          <li>Theme</li>
+          <li>
+            <Button>Post Job</Button>
+          </li>
+          <li>Profile</li>
+        </ul>
+      </nav>
+    </Container>
   );
 };
 
