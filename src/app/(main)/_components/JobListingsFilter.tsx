4e519a56f17a733bb651c6781785e85c4825a678
osteensouthpaw <osteensouthpaw@gmail.com>
2025-03-19 12:31:44 +0300

    build filter component

---
+++ src/app/(main)/_components/JobListingsFilter.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { RadioGroupItem, RadioGroup } from "@/components/ui/radio-group";
+import { ExperienceLevel, JobType, WorkMode } from "@/services/jobPost-service";
+import { useState } from "react";
+
+const jobTypes: { key: JobType; label: string }[] = [
+  { key: JobType.CONTRACT, label: "Contract" },
+  { key: JobType.FULL_TIME, label: "Full-Time" },
+  { key: JobType.INTERNSHIP, label: "Internship" },
+];
+
+const experienceLevels: { key: ExperienceLevel; label: string }[] = [
+  { key: ExperienceLevel.ENTRY_LEVEL, label: "Entry" },
+  { key: ExperienceLevel.JUNIOR, label: "Junior" },
+  { key: ExperienceLevel.SENIOR, label: "Senior" },
+  { key: ExperienceLevel.PROFESSIONAL, label: "Professional" },
+];
+
+const workModes: { key: WorkMode; label: string }[] = [
+  { key: WorkMode.HYBRID, label: "Hybrid" },
+  { key: WorkMode.REMOTE, label: "Remote" },
+  { key: WorkMode.ON_SITE, label: "On Site" },
+];
+
+const jobListingsFilter = () => {
+  return (
+    <Card className="px-3">
+      <CardHeader className="font-semibold text-2xl">Filter</CardHeader>
+      <CardContent className="space-y-8">
+        <JobFilter title="Job Type" filterTypes={jobTypes} />
+        <JobFilter title="Experience Level" filterTypes={experienceLevels} />
+        <JobFilter title="Work Mode" filterTypes={workModes} />
+      </CardContent>
+    </Card>
+  );
+};
+
+interface Props {
+  title: string;
+  filterTypes: typeof jobTypes | typeof workModes | typeof experienceLevels;
+}
+
+const JobFilter = ({ title, filterTypes }: Props) => {
+  return (
+    <div className="space-y-2">
+      <Label className="font-semibold text-lg">{title}</Label>
+      <RadioGroup className="flex gap-2">
+        {filterTypes.map((filter) => (
+          <div className="flex items-center space-x-2" key={filter.label}>
+            <RadioGroupItem value={filter.key} id={filter.key} />
+            <Label htmlFor={filter.key}>{filter.label}</Label>
+          </div>
+        ))}
+      </RadioGroup>
+    </div>
+  );
+};
+
+export default jobListingsFilter;
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import JobListings from "./_components/JobListings";
+import JobListingsFilter from "./_components/JobListingsFilter";
 
 const HomePage = () => {
-  return <JobListings />;
+  return (
+    <div className="grid grid-cols-3 gap-8 mt-10">
+      <JobListingsFilter />
+      <div className="col-span-2 col-start-2">
+        <JobListings />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
