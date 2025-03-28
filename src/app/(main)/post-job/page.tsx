2645e955d06f4b33d0cc580215a712f6618963c4
osteensouthpaw <osteensouthpaw@gmail.com>
2025-03-28 09:35:20 +0300

    add the testimonial section on the post job page

--- src/app/(main)/post-job/page.tsx
+++ src/app/(main)/post-job/page.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import JobPostForm from "./_components/JobPostForm";
 import { Card } from "@/components/ui/card";
+import Testimonials from "./_components/Testimonials";
 
 const NewJobPostPage = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-10">
       <div className="lg:col-span-2">
         <Card>
           <JobPostForm />
         </Card>
       </div>
-      <div className="">
-        <Card></Card>
+      <div className="col-span-1">
+        <Testimonials />
       </div>
     </div>
   );
