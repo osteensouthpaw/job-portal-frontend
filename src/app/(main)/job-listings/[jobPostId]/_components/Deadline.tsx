15a3fd7d44a9f7f1bd9954fb4b23f02d15ce4b71
osteensouthpaw <osteensouthpaw@gmail.com>
2025-03-28 07:01:50 +0300

    fix application deadline not working

--- src/app/(main)/job-listings/[jobPostId]/_components/Deadline.tsx
+++ src/app/(main)/job-listings/[jobPostId]/_components/Deadline.tsx
@@ -1,12 +1,14 @@
 import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
+import { differenceInDays } from "date-fns";
 
-const Deadline = () => {
+const Deadline = ({ deadline }: { deadline: string }) => {
+  const days = differenceInDays(new Date(), new Date(deadline)).toString();
   return (
     <Card>
       <Badge variant="outline" className="size-24 grid place-content-center">
         <div className="flex flex-col gap-2 items-center">
-          <p className="font-semibold text-5xl">23</p>
+          <p className="font-semibold text-5xl">{days}</p>
           <hr />
           <p className="text-center font-normal text-lg">Days Left</p>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/general/NavBar";
-import { Container } from "@/components/ui/container";
-import { ThemeProvider } from "@/components/ui/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-muted `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-muted dark:bg-background`}
       >
         <ThemeProvider
           attribute="class"
