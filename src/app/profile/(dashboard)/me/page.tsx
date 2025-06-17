739423ff07b511a5aa3a3d030eabf9aac4a97bec
osteensouthpaw <osteensouthpaw@gmail.com>
2025-06-17 08:13:33 +0300

    feat: enhance UserProfilePage to display incomplete profile message with a card layout

--- src/app/profile/(dashboard)/me/page.tsx
+++ src/app/profile/(dashboard)/me/page.tsx
@@ -3,6 +3,13 @@ import { findJobSeekerProfile } from "@/services/profile-service";
 import { cookies } from "next/headers";
 import UserProfile from "../../../profile/_components/UserProfile";
 import { redirect } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const UserProfilePage = async () => {
   const cookieHeader = (await cookies()).toString();
@@ -10,9 +17,22 @@ const UserProfilePage = async () => {
   if (!user) return redirect("/auth/login");
 
   const jobSeekerProfile = await findJobSeekerProfile(user.id, cookieHeader);
-  console.log(jobSeekerProfile);
-
-  return <UserProfile jobSeekerProfile={jobSeekerProfile} />;
+  return jobSeekerProfile ? (
+    <UserProfile jobSeekerProfile={jobSeekerProfile} />
+  ) : (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold">Profile</CardTitle>
+        <CardDescription>
+          Your profile is not yet complete. Click this link below to complete
+          your profile for it to be visible to employers and hiring managers.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Continue with this link to complete your profile:</p>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default UserProfilePage;
