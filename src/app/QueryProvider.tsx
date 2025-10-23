79062ae47e4dbeec2279adab14fcf3a1819209d5
osteensouthpaw <osteensouthpaw@gmail.com>
2025-10-23 20:30:47 +0300

    configure react query dev tools

--- src/app/QueryProvider.tsx
+++ src/app/QueryProvider.tsx
@@ -1,11 +1,23 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const QueryProvider = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const options = {
+    defaultOptions: {
+      queries: {
+        retry: 3,
+        staleTime: 10 * 1000,
+      },
+    },
+  };
+  const [queryClient] = useState(() => new QueryClient(options));
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 
